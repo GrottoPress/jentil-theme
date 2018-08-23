a21df9367a10afa8e3df2da15d2fb6ffa047b0e8
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2018-08-23 18:47:48 +0000

    Comment out code

--- assets/scripts/customize-preview.ts
+++ assets/scripts/customize-preview.ts
@@ -11,23 +11,23 @@
         })
     })
 
-    customize('background_color', (from: () => void): void => {
-        from.bind((to: string): void => {
-            if (-1 === ['#fff', '#ffffff'].indexOf(to)) {
-                $('body').removeClass('no-background-color').addClass('has-background-color')
-            } else {
-                $('body').removeClass('has-background-color').addClass('no-background-color')
-            }
-        })
-    })
+    // customize('background_color', (from: () => void): void => {
+    //     from.bind((to: string): void => {
+    //         if (-1 === ['#fff', '#ffffff'].indexOf(to)) {
+    //             $('body').removeClass('no-background-color').addClass('has-background-color')
+    //         } else {
+    //             $('body').removeClass('has-background-color').addClass('no-background-color')
+    //         }
+    //     })
+    // })
 
-    customize('background_image', (from: () => void): void => {
-        from.bind((to: string): void => {
-            if (to) {
-                $('body').removeClass('no-background-image').addClass('has-background-image')
-            } else {
-                $('body').removeClass('has-background-image').addClass('no-background-image')
-            }
-        })
-    })
+    // customize('background_image', (from: () => void): void => {
+    //     from.bind((to: string): void => {
+    //         if (to) {
+    //             $('body').removeClass('no-background-image').addClass('has-background-image')
+    //         } else {
+    //             $('body').removeClass('has-background-image').addClass('no-background-image')
+    //         }
+    //     })
+    // })
 })(wp, jQuery)
